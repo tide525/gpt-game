9776a348a44fe2665b3d1506ac5c2c8b0fb2852c
tide525 <tide525@outlook.jp>
2023-10-16 21:56:33 +0900

    [update] テキストをよりリッチに

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,17 @@ import * as Phaser from "phaser";
 const WIDTH = 360;
 const HEIGHT = 640;
 
-const MAX_RADIUS = 120;
-const IMAGE_RADIUS = 160;
+const SCORE_MARGIN = 20;
 
+const INIT_BALL_Y = 80;
+const DROPPING_SECONDS = 1;
+
+const IMAGE_RADIUS = 160;
+const MAX_RADIUS = 120;
 const FRICTION = 0.01;
 const BOUNCE = 0.1;
 
-const DROPPING_SECONDS = 1;
-const INIT_BALL_Y = 80;
-
-const UPPER_BOUND = 100;
+const UPPER_BOUND_Y = 100;
 
 // GPT族のパラメータ数
 // 後半は噂でしかないから適当
@@ -40,11 +41,13 @@ function expWeightedRandom(n: number) {
 }
 
 export default class Game extends Phaser.Scene {
-  nextBall: Phaser.GameObjects.Image = null;
-  isDropping: boolean = false;
-  maxColor: number = 0;
+  scales: number[];
+  nextBall: Phaser.GameObjects.Image;
 
-  currentBalls: Phaser.Physics.Matter.Image[];
+  isDropping: boolean = false;
+
+  currentBalls: Phaser.Physics.Matter.Image[] = [];
+  maxLevel: number = 0;
 
   score: number = 0;
   scoreText: Phaser.GameObjects.Text;
@@ -60,6 +63,49 @@ export default class Game extends Phaser.Scene {
   }
 
   create() {
+    this.scales = this.calcScales();
+
+    this.add.rectangle(
+      WIDTH / 2,
+      UPPER_BOUND_Y / 2,
+      WIDTH,
+      UPPER_BOUND_Y,
+      0x202123
+    );
+    this.add.rectangle(
+      WIDTH / 2,
+      (HEIGHT - UPPER_BOUND_Y) / 2 + UPPER_BOUND_Y,
+      WIDTH,
+      HEIGHT - UPPER_BOUND_Y,
+      0xf7f7f8
+    );
+
+    // 操作説明
+    this.scoreText = this.add
+      .text(WIDTH / 2, HEIGHT / 2, "クリックでGPTを落とす")
+      .setColor("#111827")
+      .setAlpha(0.5)
+      .setOrigin(0.5, 0.5);
+    this.scoreText = this.add
+      .text(WIDTH / 2, HEIGHT / 2 + UPPER_BOUND_Y, "同じGPTがぶつかると進化")
+      .setColor("#111827")
+      .setAlpha(0.5)
+      .setOrigin(0.5, 0.5);
+
+    // スコア表示
+    this.add
+      .text(SCORE_MARGIN, SCORE_MARGIN, "スコア")
+      .setColor("#ececf1")
+      .setOrigin(0, 0);
+    this.scoreText = this.add
+      .text(WIDTH - SCORE_MARGIN, SCORE_MARGIN, `${this.score}パラメータ`)
+      .setColor("#ececf1")
+      .setOrigin(1, 0);
+    this.add
+      .text(SCORE_MARGIN, UPPER_BOUND_Y, "メモリここまで↓")
+      .setColor("#ececf1")
+      .setOrigin(0, 1);
+
     this.matter.world.setBounds(
       0,
       0,
@@ -72,87 +118,17 @@ export default class Game extends Phaser.Scene {
       true
     );
 
-    this.add.rectangle(
-      WIDTH / 2,
-      UPPER_BOUND / 2,
-      WIDTH,
-      UPPER_BOUND,
-      0x202123
-    );
-    this.add.rectangle(
-      WIDTH / 2,
-      (HEIGHT - UPPER_BOUND) / 2 + UPPER_BOUND,
-      WIDTH,
-      HEIGHT - UPPER_BOUND,
-      0xf7f7f8
-    );
-
-    // スコア表示
-    this.add.text(20, 20, "スコア").setColor("#ececf1");
-    this.scoreText = this.add
-      .text(WIDTH - 20, 20, `${this.score}パラメータ`)
-      .setColor("#ececf1")
-      .setOrigin(1, 0);
-
-    this.currentBalls = [];
-
     // 最初のボール
     this.createNextBall();
 
     // クリックで落とす
-    this.input.on("pointerdown", this.handlePointerdown, this);
+    this.input.on("pointerup", this.handlePointerup, this);
 
-    this.matter.world.on(
-      "collisionstart",
-      (event: any, bodyA: MatterJS.BodyType, bodyB: MatterJS.BodyType) => {
-        // 壁との衝突は無視
-        if (bodyA.gameObject === null || bodyB.gameObject === null) {
-          return;
-        }
-
-        // ボール同士
-        const ballA: Phaser.Physics.Matter.Image = bodyA.gameObject;
-        const ballB: Phaser.Physics.Matter.Image = bodyB.gameObject;
-
-        const colorA: number = ballA.getData("color");
-        const colorB: number = ballB.getData("color");
-        if (colorA !== colorB) {
-          return;
-        }
-
-        const centerX = (ballA.x + ballB.x) / 2;
-        const centerY = (ballA.y + ballB.y) / 2;
-
-        // 既存のボールを削除
-        ballA.setVisible(false);
-        ballB.setVisible(false);
-        this.matter.world.remove([ballA, ballB]);
-
-        this.score += PARAMS[colorA] * 1000000000;
-
-        // 結合したボールを追加
-        if (colorA === PARAMS.length) {
-          return;
-        }
-
-        const newColor = colorA + 1;
-        const ball = this.createBall(centerX, centerY, newColor);
-
-        if (newColor > this.maxColor) {
-          this.maxColor = newColor;
-        }
-      },
-      this
-    );
+    this.matter.world.on("collisionstart", this.handleCollision, this);
   }
 
   update() {
-    const pointer = this.input.activePointer;
-
-    if (this.nextBall !== null) {
-      this.nextBall.setPosition(pointer.x, INIT_BALL_Y);
-    }
-
+    this.updateNextBall();
     this.scoreText.setText(`${this.score}パラメータ`);
   }
 
@@ -173,52 +149,93 @@ export default class Game extends Phaser.Scene {
     return scales;
   }
 
-  createNextBall() {
-    const nextBallColor = expWeightedRandom(Math.max(this.maxColor, 1));
+  handleCollision(
+    event: any,
+    bodyA: MatterJS.BodyType,
+    bodyB: MatterJS.BodyType
+  ) {
+    // 壁との衝突は無視
+    if (bodyA.gameObject === null || bodyB.gameObject === null) {
+      return;
+    }
 
-    const texture = TEXTURES[nextBallColor];
-    const scale = this.calcScales()[nextBallColor];
+    // ボール同士
+    const ballA: Phaser.GameObjects.Image = bodyA.gameObject;
+    const ballB: Phaser.GameObjects.Image = bodyB.gameObject;
 
-    this.nextBall = this.add
-      .image(160, INIT_BALL_Y, texture)
+    const levelA: number = ballA.getData("level");
+    const levelB: number = ballB.getData("level");
+    if (levelA !== levelB) {
+      return;
+    }
 
-      .setData("color", nextBallColor)
-      .setScale(scale);
+    const centerX = (ballA.x + ballB.x) / 2;
+    const centerY = (ballA.y + ballB.y) / 2;
+
+    // 既存のボールを削除
+    this.score += PARAMS[levelA] * 1000000000;
+
+    this.matter.world.remove([ballA, ballB]);
+    ballA.setVisible(false);
+    ballB.setVisible(false);
+
+    // 結合したボールを追加
+    if (levelA === PARAMS.length - 1) {
+      return;
+    }
+    this.createBall(centerX, centerY, levelA + 1);
   }
 
-  createBall(x: number, y: number, color: number) {
-    const texture = TEXTURES[color];
-    const scale = this.calcScales()[color];
+  createNextBall() {
+    // 今ある最大よりも一回り小さいボール
+    const nextBallLevel = expWeightedRandom(Math.max(this.maxLevel, 1));
+
+    const texture = TEXTURES[nextBallLevel];
+    const scale = this.scales[nextBallLevel];
+
+    this.nextBall = this.add
+      .image(WIDTH / 2, INIT_BALL_Y, texture)
+      .setScale(scale)
+      .setData("level", nextBallLevel);
+  }
+
+  updateNextBall() {
+    const pointer = this.input.activePointer;
+    this.nextBall.setPosition(pointer.x, INIT_BALL_Y);
+  }
+
+  createBall(x: number, y: number, level: number) {
+    const texture = TEXTURES[level];
+    const scale = this.scales[level];
 
     const ball = this.matter.add
       .image(x, y, texture)
-
       .setCircle(IMAGE_RADIUS)
       .setFriction(FRICTION)
       .setBounce(BOUNCE)
-
-      .setData("color", color)
-      .setScale(scale);
+      .setScale(scale)
+      .setData("level", level);
 
     this.currentBalls.push(ball);
 
-    return ball;
+    if (level > this.maxLevel) {
+      this.maxLevel = level;
+    }
   }
 
-  handlePointerdown() {
+  handlePointerup() {
     if (this.isDropping) {
       return;
     }
 
     // ボールを実体化
+    this.nextBall.setVisible(false);
+
     this.createBall(
       this.nextBall.x,
       this.nextBall.y,
-      this.nextBall.getData("color")
+      this.nextBall.getData("level")
     );
-
-    this.nextBall.setVisible(false);
-    this.nextBall = null;
 
     // 次のボールを出す
     this.isDropping = true;
@@ -231,52 +248,10 @@ export default class Game extends Phaser.Scene {
   }
 
   dropCallback() {
-    // ゲームオーバーを確認
+    // ゲームオーバー確認
     for (const ball of this.currentBalls) {
-      if (ball.y < UPPER_BOUND) {
-        this.add.rectangle(WIDTH / 2, HEIGHT / 2, WIDTH, HEIGHT, 0xffffff, 0.8);
-
-        this.add
-          .text(WIDTH / 2, HEIGHT / 2, "ゲームオーバー")
-          .setColor("#111827")
-          .setFontSize(48)
-          .setFontStyle("bold")
-          .setOrigin(0.5, 0.5);
-        this.input.off("pointerdown", this.handlePointerdown, this);
-
-        this.add
-          .text(WIDTH / 2, HEIGHT / 2 + 60, `${this.score}パラメータ`)
-          .setFontSize(24)
-          .setFontStyle("bold")
-          .setColor("#111827")
-          .setOrigin(0.5, 0.5);
-
-        this.add
-          .text(WIDTH / 2, HEIGHT / 2 + 100, "結果をXで共有")
-          .setColor("#111827")
-          .setFontSize(24)
-          .setFontStyle("bold")
-          .setOrigin(0.5, 0.5)
-          .setInteractive()
-          .on(
-            "pointerdown",
-            function () {
-              window.open(
-                `https://twitter.com/intent/tweet?text=${this.score}パラメータをメモリに載せられたよ&url=https://tide525.github.io/gpt-game/&hashtags=GPTゲーム`,
-                "_blank",
-                "noopener,noreferrer"
-              );
-            },
-            this
-          );
-
-        this.add
-          .text(WIDTH / 2, HEIGHT / 2 + 200, "リロードでリトライ")
-          .setFontSize(24)
-          .setFontStyle("bold")
-          .setColor("#111827")
-          .setOrigin(0.5, 0.5);
-
+      if (ball.y < UPPER_BOUND_Y) {
+        this.handleOver();
         return;
       }
     }
@@ -285,6 +260,50 @@ export default class Game extends Phaser.Scene {
     this.isDropping = false;
 
     this.createNextBall();
+  }
+
+  handleOver() {
+    this.add.rectangle(WIDTH / 2, HEIGHT / 2, WIDTH, HEIGHT, 0xffffff, 0.8);
+
+    this.add
+      .text(WIDTH / 2, HEIGHT / 2, "ゲームオーバー")
+      .setColor("#111827")
+      .setFontSize(24)
+      .setFontStyle("bold")
+      .setOrigin(0.5, 0.5);
+    this.input.off("pointerup", this.handlePointerup, this);
+
+    // スコア表示
+    this.add
+      .text(
+        WIDTH / 2,
+        HEIGHT / 2 + UPPER_BOUND_Y * 0.5,
+        `${this.score}パラメータが載っかった`
+      )
+      .setColor("#111827")
+      .setOrigin(0.5, 0.5);
+
+    this.add
+      .text(WIDTH / 2, HEIGHT / 2 + UPPER_BOUND_Y * 1.5, "結果をXで共有")
+      .setColor("#111827")
+      .setOrigin(0.5, 0.5)
+      .setInteractive()
+      .on(
+        "pointerup",
+        function () {
+          window.open(
+            `https://twitter.com/intent/tweet?text=${this.score}パラメータをメモリに載っけたよ&url=https://tide525.github.io/gpt-game/&hashtags=GPTゲーム`,
+            "_blank",
+            "noopener,noreferrer"
+          );
+        },
+        this
+      );
+
+    this.add
+      .text(WIDTH / 2, HEIGHT / 2 + UPPER_BOUND_Y * 2, "リロードでリトライ")
+      .setColor("#111827")
+      .setOrigin(0.5, 0.5);
   }
 }
 
